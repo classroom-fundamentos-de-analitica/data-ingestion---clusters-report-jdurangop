0be66947bed52987897b3888690f024311b1d25d
jdurangop <jdurangop@unal.edu.co>
2022-10-17 16:01:06 -0500

    prueba1

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,72 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
+def getNewLine(line, colSpecs):
+    newLine = list()
+
+    for start, end in colSpecs[:-1]:
+        newLine.append(line[start:end].strip())
+    
+    last = colSpecs[-1][0]
+    newLine.append(line[last:].strip())
+    
+    newLine[2] = newLine[2].strip(' %').replace(',', '.')
+    
+    return ';'.join(newLine)
 
 def ingest_data():
 
     #
     # Inserte su código aquí
     #
+    with open('clusters_report.txt', mode='r') as file_data:
+        clustersData = file_data.readlines()
+        colNames = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave']
+
+        ## getting column indexes ##
+        colSpecs = list()
+        pos = 0
+
+        firstLine = clustersData[0]
+        for i in range(3, len(firstLine)):
+            if firstLine[i] != " " and firstLine[i] != "\n" and firstLine[i-2:i] == "  ":
+                colSpecs.append((pos, i))
+                pos = i
+
+            if len(colSpecs) >= len(colNames) - 1:
+                break
+        
+        colSpecs.append((pos, -1))
+        
+        ## creating clean data file ##
+        with open('cleanData.txt', 'w') as cleanData:
+            #regex to combine several spaces into one
+            combineSpaces = re.compile(r"\s+")
+            
+            firstCols = ''
+            tags = ''
+
+            for line in clustersData[4:]:
+                if line.strip() != '':
+                    if firstCols == '':
+                        firstCols = line[:pos]
+                    
+                    tags += line[pos:].strip() + ' '
+                    continue
+
+                tags = combineSpaces.sub(" ", tags).strip()
+
+                if tags[-1] == '.':
+                    tags = tags[:-1]
+
+                newLine = getNewLine(firstCols + tags, colSpecs)
+                cleanData.write(newLine + '\n')
+
+                firstCols = ''
+                tags = ''
+    
+    df = pd.read_csv('cleanData.txt', sep = ';', names = colNames)
 
     return df
